81ebcbf6806ca8d22e50526c810ebac74ba74265
Ville Penttinen <villem.penttinen@gmail.com>
2024-02-24 12:07:12 +0000

    Refactor GetResultsInFullPath

--- src/Vipentti.IO.Abstractions.FileSystemGlobbing/MatcherExtensions.cs
+++ src/Vipentti.IO.Abstractions.FileSystemGlobbing/MatcherExtensions.cs
@@ -22,7 +22,7 @@ public static class MatcherExtensions
     /// <param name="matcher">The matcher</param>
     /// <param name="fileSystem">The filesystem</param>
     /// <param name="directoryPath">The root directory for the search</param>
-    /// <returns>Always returns instance of <see cref="PatternMatchingResult" />, even if not files were matched</returns>
+    /// <returns>Always returns instance of <see cref="PatternMatchingResult" />, even if no files were matched</returns>
     public static PatternMatchingResult Execute(
         this Matcher matcher,
         IFileSystem fileSystem,
@@ -49,23 +49,6 @@ public static class MatcherExtensions
         return matcher.Execute(new DirectoryInfoGlobbingWrapper(fileSystem, directoryInfo));
     }
 
-    /// <inheritdoc cref="GetResultsInFullPath(Matcher, IFileSystem, string)"/>
-    public static IEnumerable<string> GetResultsInFullPath(
-        this Matcher matcher,
-        IFileSystem fileSystem,
-        IDirectoryInfo directoryInfo
-    )
-    {
-        ThrowHelpers.ThrowIfNull(matcher);
-        ThrowHelpers.ThrowIfNull(fileSystem);
-        ThrowHelpers.ThrowIfNull(directoryInfo);
-
-        var matches = Execute(matcher, fileSystem, directoryInfo).Files;
-        return matches
-            .Select(match => Path.GetFullPath(Path.Combine(directoryInfo.FullName, match.Path)))
-            .ToArray();
-    }
-
     /// <summary>
     /// Searches the directory specified for all files matching patterns added to this instance of <see cref="Matcher" />
     /// </summary>
@@ -88,4 +71,33 @@ public static class MatcherExtensions
             fileSystem.DirectoryInfo.New(directoryPath)
         );
     }
+
+    /// <inheritdoc cref="GetResultsInFullPath(Matcher, IFileSystem, string)"/>
+    public static IEnumerable<string> GetResultsInFullPath(
+        this Matcher matcher,
+        IFileSystem fileSystem,
+        IDirectoryInfo directoryInfo
+    )
+    {
+        ThrowHelpers.ThrowIfNull(matcher);
+        ThrowHelpers.ThrowIfNull(fileSystem);
+        ThrowHelpers.ThrowIfNull(directoryInfo);
+
+        var matches = Execute(matcher, fileSystem, directoryInfo);
+
+        if (!matches.HasMatches)
+        {
+            return EmptyStringsEnumerable;
+        }
+
+        var fsPath = fileSystem.Path;
+        var directoryFullName = directoryInfo.FullName;
+
+        return matches.Files.Select(GetFullPath);
+
+        string GetFullPath(FilePatternMatch match) =>
+            fsPath.GetFullPath(fsPath.Combine(directoryFullName, match.Path));
+    }
+
+    private static readonly IEnumerable<string> EmptyStringsEnumerable = Enumerable.Empty<string>();
 }
